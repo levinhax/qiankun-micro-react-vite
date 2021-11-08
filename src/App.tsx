ddb572c69d0094ac8fc99bae875a2f21c7ab05b2
levinhax <levin1229@163.com>
2021-11-08 16:03:49 +0800

    feat: 引入路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './styles/index.css'
+import { Divider } from 'antd'
+
+import Home from './views/Home'
+const About = lazy(() => import('./views/About'))
+
+const RouteEle = () => {
+  return (
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Divider type="vertical" />
+        <Link to="/about" style={{ padding: 5 }}>
+          About
+        </Link>
+      </nav>
+
+      <Suspense fallback={null}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <RouteEle />
     </div>
   )
 }
---
+++ src/views/About/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const About = () => {
+  return <div className="about-wrapper">About 测试</div>
+}
+
+export default About
