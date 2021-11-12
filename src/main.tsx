c06108cac8236e30e3f545d91d47979f0dcb3850
levinhax <levin1229@163.com>
2021-11-12 14:35:41 +0800

    style: antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import 'antd/dist/antd.css'
+import './styles/index.css'
+import './styles/app.css'
 import App from './App'
 
 // vite-plugin-qiankun helper
